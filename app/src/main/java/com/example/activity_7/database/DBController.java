ba4170ccfe20eb01363c2c97642a674c7fc37025
yamz-11 <wiratamajrz@gmail.com>
2022-04-13 20:46:06 +0700

    Delete and Update Data SQLite

---
+++ app/src/main/java/com/example/activity_7/database/DBController.java
@@ -0,0 +1,84 @@
+package com.example.activity_7.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DBController extends SQLiteOpenHelper {
+
+    public DBController(Context context) {
+        super(context, "ProdiTI", null, 1);
+    }
+
+    // Membuat tabel database
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE teman (id integer primary key, nama text, telpon text)");
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        // delete tabel
+        db.execSQL("DROP TABLE if exists teman");
+
+        onCreate(db);
+    }
+
+    public void  insertData(HashMap<String,String> queryValues){
+
+        // Variable SQLiteDatabase
+        SQLiteDatabase basisdata = this.getWritableDatabase();
+
+        ContentValues nilai = new ContentValues();
+        nilai.put("nama",queryValues.get("nama"));
+        nilai.put("telpon", queryValues.get("telpon"));
+
+        basisdata.insert("teman",null,nilai);
+        basisdata.close();
+    }
+
+    public void UpdateData(HashMap<String,String> queryValues){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues nilai = new ContentValues();
+        nilai.put("nama", queryValues.get("nama"));
+        nilai.put("telpon", queryValues.get("telpon"));
+        db.update("teman", nilai, "id=?",new String[]{queryValues.get("id")});
+        db.close();
+    }
+
+    public void DeleteData(HashMap<String,String> queryValue){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete("teman","id=?",new String[]{queryValue.get("id")});
+        db.close();
+    }
+
+    public ArrayList<HashMap<String,String>> getAllTeman(){
+        ArrayList<HashMap<String,String>> daftarTeman;
+        daftarTeman = new ArrayList<HashMap<String,String>>();
+
+        String selectQuery = " SELECT * FROM teman";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+
+        if(cursor.moveToFirst())
+        {
+            do {
+                HashMap<String,String> map = new  HashMap<>();
+                map.put("id", cursor.getString(0));
+                map.put("nama", cursor.getString(1));
+                map.put("telpon", cursor.getString(2));
+                daftarTeman.add(map);
+            }
+            while (cursor.moveToNext());
+        }
+        db.close();
+        return daftarTeman;
+    }
+}
